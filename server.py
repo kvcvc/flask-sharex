0e6db078e0c4da500776204dc31b1fce82f773b6
Marko Kovačević <markokovacevic@email.cz>
2018-05-21 20:30:50 +0200

    Better privacy and docstrings
    No longer save file with metadata for privacy reasons. Added docstrings for future documentation.

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, json
+from PIL import Image
 
 
 import os
@@ -18,10 +19,12 @@ app = Flask(__name__)
 def upload():
     if request.method == 'POST':
         if request.form.to_dict(flat=False)['secret_key'][0] == secret_key:
+            '''Get file object from POST request, extract and define needed variables for future use.'''
             file = request.files['image']
             extension = splitext(file.filename)[1]
             file.flush()
             size = os.fstat(file.fileno()).st_size
+            '''Check for file extension and file size.'''
             if extension not in allowed_extension:
                 return 'File type is not supported', 415
 
@@ -29,8 +32,15 @@ def upload():
                 return 'File size too large', 400
 
             else:
+                '''Remove metadata of the file.'''
+                image = Image.open(file)
+                data = list(image.getdata())
+                file_without_exif = Image.new(image.mode, image.size)
+                file_without_exif.putdata(data)
+
+                '''Save the image with a new randomly generated filename in the desired path, and return URL info.'''
                 filename = secrets.token_urlsafe(5)
-                file.save(os.path.join(storage_folder, filename + extension))
+                file_without_exif.save(os.path.join(storage_folder, filename + extension))
                 return json.dumps({"filename": filename, "extension": extension}), 200
         else:
             return 'Unauthorized use', 401
