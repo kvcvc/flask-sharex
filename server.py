80fef0ba5447515782b6ec1bd7575d0c855784ea
Marko Kovačević <markokovacevic@email.cz>
2018-05-21 20:15:11 +0200

    Better security and ShareX config
    Added checks for file extensions and file sizes in order to improve security. I also included the config if you want to use this destination with ShareX.

--- server.py
+++ server.py
@@ -8,6 +8,7 @@ import secrets
 
 storage_folder = 'STORAGE FOLDER'
 secret_key = 'SECRET'
+allowed_extension = ['.png', '.jpeg', '.jpg', '.gif']
 
 
 app = Flask(__name__)
@@ -19,9 +20,20 @@ def upload():
         if request.form.to_dict(flat=False)['secret_key'][0] == secret_key:
             file = request.files['image']
             extension = splitext(file.filename)[1]
-            filename = secrets.token_urlsafe(5)
-            file.save(os.path.join(storage_folder, filename + extension))
-            return json.dumps({"filename": filename, "extension": extension}), 200
+            file.flush()
+            size = os.fstat(file.fileno()).st_size
+            if extension not in allowed_extension:
+                return 'File type is not supported', 415
+
+            elif size > 6000000:
+                return 'File size too large', 400
+
+            else:
+                filename = secrets.token_urlsafe(5)
+                file.save(os.path.join(storage_folder, filename + extension))
+                return json.dumps({"filename": filename, "extension": extension}), 200
+        else:
+            return 'Unauthorized use', 401
 
 
 if __name__ == '__main__':
